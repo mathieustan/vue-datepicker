d5c1c3bc65fec141b882e21334ed8ab303bbf99c
Stan <mathieu.stan@gmail.com>
2019-11-08 09:21:13 +0100

    :pencil: Added github link to doc

--- doc-src/src/main.js
+++ doc-src/src/main.js
@@ -4,8 +4,6 @@ import App from './App.vue';
 import VueDatePicker from '@mathieustan/vue-datepicker'; // Prod
 // import VueDatePicker from '../../src'; // Local
 
-import './styles/main.scss';
-
 Vue.use(VueDatePicker);
 
 Vue.config.productionTip = false;
