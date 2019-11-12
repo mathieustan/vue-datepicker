8891f0698350b2a21096a4133ad4b9bbfcb7238c
Stan <mathieu.stan@gmail.com>
2019-11-12 09:56:42 +0100

    :bug: Allow to import VueDatePicker as component (fix #30)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import * as components from './components';
 import VueDatePicker from './framework';
 
+export * from './components';
 export default VueDatePicker;
 
 const install = VueDatePicker.install;
-
 VueDatePicker.install = (Vue, options) => {
   install.call(VueDatePicker, Vue, {
     components,
