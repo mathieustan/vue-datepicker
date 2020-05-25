7bbbfaf2c34dd3a554c0cd02cc7462100a99c972
Stan <mathieu.stan@gmail.com>
2020-05-25 10:28:27 +0200

    :package: Updated rollup dep & added snapshot sizes

--- build/rollup.config.base.js
+++ build/rollup.config.base.js
@@ -1,14 +1,16 @@
-import path from 'path';
-import buble from 'rollup-plugin-buble';
-import sizes from 'rollup-plugin-sizes';
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import builtins from 'rollup-plugin-node-builtins';
-import vue from 'rollup-plugin-vue';
-import commonjs from 'rollup-plugin-commonjs';
-import replace from 'rollup-plugin-replace';
-import eslint from 'rollup-plugin-eslint';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 import { terser } from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
+import buble from '@rollup/plugin-buble';
+import builtins from 'rollup-plugin-node-builtins';
+import cleanup from 'rollup-plugin-cleanup';
+import commonjs from '@rollup/plugin-commonjs';
+import eslint from 'rollup-plugin-eslint';
+import path from 'path';
+import postcss from 'rollup-plugin-postcss';
+import replace from '@rollup/plugin-replace';
+import resolve from '@rollup/plugin-node-resolve';
+import vue from 'rollup-plugin-vue';
 
 const RESOLVE_FIELDS = ['main', 'browser', 'jsnext'];
 
@@ -18,8 +20,30 @@ const {
   version,
 } = require(path.resolve(process.cwd(), 'package.json'));
 
+const banner =
+  '/*!\n' +
+  ` * Vue-datepicker v${version}\n` +
+  ` * (c) 2019-${new Date().getFullYear()} Mathieu Stanowski\n` +
+  ' */';
+
 const plugins = [
+  // -------------------------------------------
+  // Lint files
+  // -------------------------------------------
+  eslint({ exclude: ['**/*.scss'] }),
+  // -------------------------------------------
+  // Allow node builtins
+  // -------------------------------------------
   builtins(),
+  // -------------------------------------------
+  // 1. babel
+  // 2. resolve => Locate modules for using third party modules in
+  // 3. commonjs => used with resolve whioch allow to bundle your CommonJS dependencies in node_modules.
+  // -------------------------------------------
+  babel({
+    exclude: 'node_modules/**',
+    babelHelpers: 'runtime',
+  }),
   resolve({ mainFields: RESOLVE_FIELDS }),
   commonjs({
     include: 'node_modules/**',
@@ -33,20 +57,24 @@ const plugins = [
       ],
     },
   }),
-  eslint(),
+  // -------------------------------------------
+  // 1. postcss => Extract scss from imported scss files in js
+  // 2. vue => Like vue-loader for webpack
+  // 3. replace => Allows to set package.json version in bundle
+  // -------------------------------------------
+  postcss({ extract: false, use: ['sass'] }),
   vue({ template: { isProduction: true } }),
-  babel({
-    exclude: 'node_modules/**',
-    runtimeHelpers: true,
-    babelrc: false,
-  }),
   replace({
     'process.env.NODE_ENV': JSON.stringify('production'),
     VERSION: JSON.stringify(version),
   }),
-  buble(),
-  sizes(),
-  terser(),
+  // -------------------------------------------
+  // Clean & Minify
+  // -------------------------------------------
+  cleanup(),
+  sizeSnapshot(),
+  buble(), // convert ES2015+
+  terser(), // minifiy
 ];
 
 export default {
@@ -60,6 +88,7 @@ export default {
       'get-size': 'getSize',
       'vue': 'Vue',
     },
+    banner,
   },
   plugins,
   watch: { include: 'src/**' },
